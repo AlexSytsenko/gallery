5451d8a1060a685b0296b8adb737acd0191cebd5
AlexSytsenko <sytsenko@gmail.com>
2021-12-17 08:41:03 +0200

    cleared code

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
 const slides = document.querySelectorAll(".slide");
 
-console.log("works");
-
-// for (const slide of slides) {
-//   slide.addEventListener("click", () => {
-//     slide.classList.add("active");
-//   });
-// }
-
 slides.forEach((slide) => {
   slide.addEventListener("click", () => {
     clearActiveClases();
