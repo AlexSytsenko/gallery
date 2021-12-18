1b8b4b0a04b02ed8f4a4675dc50037bad38c199f
AlexSytsenko <sytsenko@gmail.com>
2021-12-18 20:36:35 +0200

    updated slides plagin

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
-const slides = document.querySelectorAll(".slide");
-
-slides.forEach((slide) => {
-  slide.addEventListener("click", () => {
-    clearActiveClases();
-    slide.classList.add("active");
-  });
-});
 
 
-function clearActiveClases() {
+function slidesPlagin(activeSlide = 2) {
+  const slides = document.querySelectorAll(".slide");
+
+  slides[activeSlide].classList.add("active");
+
   slides.forEach((slide) => {
-    slide.classList.remove("active");
-  })
+    slide.addEventListener("click", () => {
+      clearActiveClases();
+      slide.classList.add("active");
+    });
+  });
 
+  function clearActiveClases() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
+
+slidesPlagin();
